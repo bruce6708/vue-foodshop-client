72153513008b554ec265aa7ca555aca0bffff00d
bruce6708 <670832174@qq.com>
2020-04-21 22:24:35 +0800

    添加swiper滑动库

--- src/pages/Msize.vue
+++ src/pages/Msize.vue
@@ -1,15 +1,46 @@
 <template>
-    <div>
-        Msize
-    </div>
+  <div>
+    <!--首页导航-->
+    <nav class="msite_nav">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+             <div class="swiper-slide">
+                 <a href="" class="link_to_food">
+                     <div>
+                         <!-- <img src="../assets/logo.png" alt=""> -->
+                     </div>
+                     <span>甜品饮料</span>
+                 </a>
+                   <a href="" class="link_to_food">
+                     <div>
+                         <!-- <img src="../assets/logo.png" alt=""> -->
+                     </div>
+                     <span>商超便利</span>
+                 </a>
+             </div>
+            <div class="swiper-slide">slider2</div>
+            <div class="swiper-slide">slider3</div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
+import Swiper from 'swiper'
+import 'swiper/css/swiper.min.css'
 
 export default {
-
+  mounted () {
+    var mySwiper = new Swiper('.swiper-container', {
+      autoplay: true, // 可选选项，自动滑动
+      pagination: {
+        el: '.swiper-pagination'
+      }
+    })
+  }
 }
 </script>
 
-<style lang="stylus" ref = "stylesheet/stylus">
-</style>
+<style lang="stylus" ref = "stylesheet/stylus"></style>
